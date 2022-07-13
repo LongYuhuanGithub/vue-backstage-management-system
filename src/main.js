1e2a1c4aef704aa90603ca59c916c542477d0d0a
longyuhuan <2630819701@qq.com>
2022-07-13 14:39:16 +0800

    初始化项目结构

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import axios from 'axios'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
 
-createApp(App).use(store).use(router).mount('#app')
+axios.defaults.baseURL = 'http://localhost:8080'
+// vue.config.globalProperties.$http = axios
+
+console.log(createApp(App).use(store).use(router).mount('#app'))
