097900076aa759570fb6c55a0aa0e64586c53e46
longyuhuan <2630819701@qq.com>
2022-07-13 15:31:20 +0800

    =

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
+import 'element-plus/dist/index.css'
 import axios from 'axios'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 
 axios.defaults.baseURL = 'http://localhost:8080'
-// vue.config.globalProperties.$http = axios
 
-console.log(createApp(App).use(store).use(router).mount('#app'))
+const vue = createApp(App)
+
+vue.config.globalProperties.$http = axios
+
+vue.use(store).use(router).mount('#app')
